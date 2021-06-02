4f88f9c9146937903e05d0f456aad3377fb0e24a
shilogilor90work <shilogilor1990work@gmail.com>
2021-06-02 14:47:15 +0300

    all

--- all.py
+++ all.py
@@ -3,8 +3,8 @@ import subprocess
 while True:
         chosen_number = input("Choose: 0: first record, 1: second record, 2: quit\n")
         if chosen_number == 0:
-            subprocess.call(['python rolljam2.py'])
+            subprocess.call(['python rolljam2.py', shell=True])
         elif chosen_number == 1:
-            subprocess.call(['python replay2.py'])
+            subprocess.call(['python replay2.py', shell=True])
         elif chosen_number == 2:
             break
