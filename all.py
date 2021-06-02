1f0d57f3a9ba4ff666d17729438c0973ccee08c9
shilogilor90work <shilogilor1990work@gmail.com>
2021-06-02 14:49:53 +0300

    all

--- all.py
+++ all.py
@@ -1,7 +1,7 @@
 import subprocess
 
 while True:
-        chosen_number = input("Choose: 0: first record, 1: second record, 2: quit\n")
+        chosen_number = input("\nChoose: 0: run jamming and recording, 1: run replay, 2: quit\n")
         if chosen_number == 0:
             subprocess.call('python rolljam2.py', shell=True)
         elif chosen_number == 1:
