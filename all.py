27c97ab7c63941ad7950a5c4c37513f0618a9b4c
shilogilor90work <shilogilor1990work@gmail.com>
2021-06-06 10:53:51 +0300

    typos

--- all.py
+++ all.py
@@ -5,15 +5,15 @@ freqs = [433.89e6, 433.847e6]
 if chosen_freq_number == "2":
     freqs[2] = input("Choose a frequency\n")
 
-Print("recording first")
-subprocess.call('python rolljam2.py first_record ' + freqs[chosen_freq_number], shell=True)
+print("recording first")
+subprocess.call('python rolljam2.py first_record ' + str(freqs[int(chosen_freq_number)]), shell=True)
 
-Print("recording second")
-subprocess.call('python rolljam2.py second_record ' + freqs[chosen_freq_number], shell=True)
+print("recording second")
+subprocess.call('python rolljam2.py second_record ' + str(freqs[int(chosen_freq_number)]), shell=True)
 
-Print("playing first")
-subprocess.call('python replay2.py first_record ' + freqs[chosen_freq_number], shell=True)
+print("playing first")
+subprocess.call('python replay2.py first_record ' + str(freqs[int(chosen_freq_number)]), shell=True)
 
 temp = input("click enter to run second recording")
-Print("playing second")
-subprocess.call('python replay2.py second_record ' + freqs[chosen_freq_number], shell=True)
+print("playing second")
+subprocess.call('python replay2.py second_record ' + str(freqs[int(chosen_freq_number)]), shell=True)
