36dc760c548ac58bca76efc5e272566d32f26140
shilogilor90work <shilogilor1990work@gmail.com>
2021-06-06 11:19:45 +0300

    another car

--- all.py
+++ all.py
@@ -1,8 +1,8 @@
 import subprocess
 
-chosen_freq_number = input("Choose a frequency: 0: 433.89e6, 1: 433.847e6 2: custom\n")
-freqs = [433.89e6, 433.847e6]
-if chosen_freq_number == "2":
+chosen_freq_number = input("Choose a frequency: 0: 433.89e6, 1: 433.847e6 2: 433.862e6 3:custom\n")
+freqs = [433.89e6, 433.847e6, 433.862e6]
+if chosen_freq_number == "3":
     freqs[2] = input("Choose a frequency\n")
 
 print("recording first")
